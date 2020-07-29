7b8f93da16c071e696bf04b4fb60baf20fe36bcd
Adarsh <adarsh12k@gmail.com>
2020-07-29 08:29:47 -0700

    Test

---
+++ index.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+var twet = ""
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+const Twit = require('twit')
+const notifier = require('node-notifier');
+const open = require('open');
+const franc = require('franc')
+
+const apikey = '396TwhBJkF3E6ny5JyEjvAjAm'
+const apiSecretKey = 'uzB8hv5BycVtZjiRXnyznOSiFkqF6F2LTJH53Vh5PoJC9soYn6'
+const accessToken = '2575237039-dICYl8XcCaTOl7JYcFExnhjiukUafoYNQ4GKJU7'
+const accessTokenSecret = 'MEwrVXK8EvwhpZe3gU0CD3ex1uYV9FoqfMEuwGBg6QIfc'
+
+var T = new Twit({
+  consumer_key:         apikey,
+  consumer_secret:      apiSecretKey,
+  access_token:         accessToken,
+  access_token_secret:  accessTokenSecret,
+});
+
+(async () => {
+
+    //1. GET RECENT TWEETS
+    T.get('search/tweets', { q: '#corona since:2020-03-01', count: 10000 }, function(err, data, response) {
+      const tweets = data.statuses
+      // .map(tweet => `LANG:${franc(tweet.text)}:${tweet.text}`) //CHECK LANGUAGE
+      .map(tweet => tweet.text)
+      // .filter(tweet => tweet.toLowerCase().includes('elon'));
+      console.log(tweets);
+      console.log(typeof(tweets));
+      for(var i =0;i<tweets.length;i++){
+        // if (tweets[i].split(":")[1]=="eng"){
+        //   twet = twet + "<li>" + tweets[i].split(":") + "</li>"
+        // }
+        if(franc(tweets[i])=="eng"){
+          twet = twet + "<li>" + tweets[i] + "</li>"
+        }
+      }
+    })
+
+    // //2. REAL TIME MONITORING USING STREAM (HASHTAG)
+    // var stream = T.stream('statuses/filter', { track: "coronavirus" })
+    // stream.on('tweet', function (tweet) {
+    //     console.log(tweet.text);
+    //     twet = tweet.text;
+    //     console.log('Language: ' + franc(tweet.text));
+    //     console.log('------');
+    // })
+
+    // 3. REAL TIME MONITORING USING STREAM (LOCATION)
+    // var sanFrancisco = [ '68.7', '8.4', '97.25', '37.6' ]
+    // var stream = T.stream('statuses/filter', { locations: sanFrancisco })
+    
+    // //SHOW NOTIFICATION FOR EACH RECEIVED TWEET
+    // stream.on('tweet', function (tweet) {
+    //   console.log(tweet.text);
+    //   let url = `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`
+
+    //   notifier.notify({
+    //     title: tweet.user.name,
+    //     message: tweet.text
+    //   });
+
+    //   notifier.on('click', async function(notifierObject, options, event) {
+    //     console.log('clicked');
+    //     await open(url);
+    //   });
+    // })
+})();
+
+app.get("/", function(req,res){
+    res.render("index.ejs",{demo:twet});
+});
+
+app.listen(3000,function(){
+    console.log("Server Started");
+});
